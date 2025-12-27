43b9296bc68f0ea5858bd7dccfde2a4dfeed387f
singhkuljeet242005-beep <singhkuljeet242005@gmail.com>
2025-12-27 23:42:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// 🌸 Global floating hearts generator (soft & slow)
-
-setInterval(() => {
-  const heart = document.createElement("div");
-  heart.className = "global-heart";
-  heart.innerHTML = "💗";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = (10 + Math.random() * 10) + "px";
-  document.body.appendChild(heart);
-
-  setTimeout(() => {
-    heart.remove();
-  }, 10000);
-}, 2000);
-
-// Keep your consent alert
 function noConsent() {
   alert("Aww 🥺 you HAVE to say yes… this was made with love 💖");
 }
